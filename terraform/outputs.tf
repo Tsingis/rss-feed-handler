ddde7f47de6b1a7ad5d73519f9b72c4cca342292
Jesse Kela <jessekkela@gmail.com>
2025-03-27 00:03:47 +0200

    Terraform workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "bucket_name" {
-  value = module.rss_notification.bucket_name
+  value     = module.rss_notification.bucket_name
+  sensitive = true
 }
 
 output "sns_topic_arn" {
-  value = module.rss_notification.sns_topic_arn
+  value     = module.rss_notification.sns_topic_arn
+  sensitive = true
 }
 
 output "lambda_function_arn" {
-  value = module.rss_notification.lambda_function_arn
+  value     = module.rss_notification.lambda_function_arn
+  sensitive = true
 }
