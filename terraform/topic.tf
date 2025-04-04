fc132a6c7903ec918af98fcbbdec7abe6b2177b0
Jesse Kela <jessekkela@gmail.com>
2025-04-04 08:24:21 +0300

    Remove encryption due costs :(

--- terraform/topic.tf
+++ terraform/topic.tf
@@ -1,14 +1,9 @@
 resource "aws_sns_topic" "rss_feeds_topic" {
   name              = var.topic_name
-  kms_master_key_id = aws_kms_key.rss_feeds_sns_key.arn
+  kms_master_key_id = null
 }
 
 resource "aws_sns_topic" "rss_feeds_alarm_topic" {
   name              = var.alarm_topic_name
   kms_master_key_id = null
-}
-
-resource "aws_kms_key" "rss_feeds_sns_key" {
-  description         = "KMS key for SNS encryption"
-  enable_key_rotation = true
 }
